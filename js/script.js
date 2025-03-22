c36f583dda840669af802a67804634403eb3e56e
arisd <arisdunn@gmail.com>
2025-03-22 12:42:55 -0500

    Cleaned up code

--- js/script.js
+++ js/script.js
@@ -2,14 +2,18 @@ var button = document.querySelector(".got-this");
 var modal = document.querySelector(".modal");
 var modalX = document.querySelector(".modal-x");
 
+var closeModal = function () {
+  modal.classList.remove("show-modal");
+  button.innerText = "Who's got this?";
+};
+
 button.addEventListener("click", function() {
   modal.classList.add("show-modal");
   button.innerText = "You've got this!!!";
 });
 
 modalX.addEventListener("click", function() {
-  modal.classList.remove("show-modal");
-  button.innerText = "Who's got this?";
+  closeModal();
 });
 
 document.addEventListener("keydown", function (e) {
